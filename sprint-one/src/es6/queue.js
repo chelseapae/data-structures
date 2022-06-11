e5044a1a4ca955a05161e2e9ad9397341e6e5bb6
Chelsea Pae <chelseapae@gmail.com>
2022-06-10 20:38:21 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    var array = Object.keys(this.storage);
+    var first = array[0];
+    var value = this.storage[first];
+    delete this.storage[first];
+    return value;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,28 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    var array = Object.keys(this.storage);
+    var last = array[array.length - 1];
+    var value = this.storage[last];
+    delete this.storage[last];
+    return value;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+
