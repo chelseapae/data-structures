501c1f3d95d2a835e57cf2567c4d619e02b8094c
Chelsea Pae <chelseapae@gmail.com>
2022-06-11 17:19:45 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,48 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create node using given value
+    var last = Node(value);
+    //change list.tail to equal this.tail
+    // list.tail = last;
+
+    if (list.head === null) {
+      list.tail = last;
+      list.head = last;
+    }
+
+    if (list.head) {
+      list.tail.next = last;
+      list.tail = last;
+    }
   };
 
   list.removeHead = function() {
+    //list.head.value
+    //list.head.next is to be assigned to list.head
+
+    // if (list.head === null) {
+    //   return null;
+    // }
+
+    var oldValue = list.head.value;
+    var newHead = list.head.next;
+    list.head = newHead;
+    return oldValue;
   };
 
   list.contains = function(target) {
+    //while (list has next), loop, reassign node.next to node.next
+    // create var thats equal to head
+    var current = list.head;
+    while (current !== null) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -23,7 +59,6 @@ var Node = function(value) {
 
   return node;
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
