0885f11f534abb65daa1cb9cc47c54057342034e
Chelsea Pae <chelseapae@gmail.com>
2022-06-17 14:51:52 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage[item] === undefined) {
+    this._storage[item] = 'is defined';
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item] !== undefined) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
